9dbee81fdf883e3057cab5f31b17f8bfd9c367fc
Emmanuel <olajideemmanuel250@gmail.com>
2025-09-08 17:47:31 +0100

    feat:payment

--- src/services/verify-payment.service.js
+++ src/services/verify-payment.service.js
@@ -1,5 +1,6 @@
 const verify_payment = async (reference,amount) => {
     try {
+        console.log("this is working")
         const secretKey = process.env.PAYSTACK_SECRET_KEY_LIVE 
         const sendReference = await fetch(`https://api.paystack.co/transaction/verify/${reference}`,{
             method:"GET",
@@ -8,11 +9,11 @@ const verify_payment = async (reference,amount) => {
         const {status,message,data} = await sendReference.json()
         if(!status && !data.status == 'success') throw 'payment not verified, please try again later'
         if(data.amount != amount) throw 'amount paid is not the amount required,please contact support for a refund'
+        console.log(status,dataStatus)
         return {status,dataStatus:data.status,amount}
-
     } catch (error) {
-        if(error) throw error
         console.log(error)
+        if(error) throw error
     }
 }
 
